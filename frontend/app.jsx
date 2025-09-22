879775219187b0df488e419ec7892acfea21a6f9
redvelvetliveproject <redvelvet.live.project@gmail.com>
2025-09-22 00:06:44 -0500

    Create app.jsx

---
+++ frontend/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Example from './components/Example.jsx';
+import Dashboard from './components/Dashboard.jsx';
+
+export default function App() {
+  return (
+    <main>
+      <h1>RedVelvetLive Frontend</h1>
+      <Example />
+      <Dashboard />
+    </main>
+  );
+}
