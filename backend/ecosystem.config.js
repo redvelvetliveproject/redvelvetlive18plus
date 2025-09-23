b4d4fc9fb3f61a8fa774719c6b89c7df5aa451b9
redvelvetliveproject <redvelvet.live.project@gmail.com>
2025-09-23 13:12:36 -0500

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,21 +1,13 @@
-// backend/ecosystem.config.js
 export default {
   apps: [
     {
-      name: "redvelvetlive-api",
-      script: "./src/server.js",   // entrypoint correcto
-      instances: 1,                // "1" para mantenerlo simple, puedes usar "max"
-      exec_mode: "fork",           // cluster si quieres usar todos los cores
+      name: 'redvelvetlive-api',
+      script: './src/server.js',
       watch: false,
       env: {
-        NODE_ENV: "development",
-        PORT: 3001                 // desarrollo local
-      },
-      env_production: {
-        NODE_ENV: "production",
-        PORT: 3001                 // 🔥 mantén 3001, porque tu Nginx hace proxy_pass a 3001
+        NODE_ENV: 'production',
+        PORT: 3001
       }
     }
   ]
-}
-
+};
