157cbb5e2ba245caefdb3495dcf6de33d74676cd
redvelvetliveproject <redvelvet.live.project@gmail.com>
2025-09-23 02:11:25 -0500

    Crear index.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+// frontend/pages/index.js
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", padding: "2rem" }}>
+      <Head>
+        <title>RedVelvetLive 🚀</title>
+      </Head>
+      <h1>RedVelvetLive 🚀</h1>
+      <p>Frontend funcionando en Next.js</p>
+    </div>
+  );
+}
