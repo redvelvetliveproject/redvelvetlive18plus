bc719a2b88102cae1c13920cb3d8d23cf1c03231
redvelvetliveproject <redvelvet.live.project@gmail.com>
2025-10-10 17:37:32 -0500

    Update logger.js

--- backend/config/logger.js
+++ backend/config/logger.js
@@ -1,11 +1,12 @@
 import pino from 'pino';
 
 const logger = pino({
-  level: process.env.LOG_LEVEL || 'info',
-  transport: {
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  transport: process.env.NODE_ENV === 'production' ? undefined : {
     target: 'pino-pretty',
     options: {
       colorize: true,
+      translateTime: 'SYS:standard',
       ignore: 'pid,hostname',
     },
   },
